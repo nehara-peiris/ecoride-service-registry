47508d99a36e53b4d12f1e06de7472f64b9ec2d3
nehara-peiris <shewmipeiris123@gmail.com>
2026-03-18 02:15:38 +0530

    eureka server done

--- src/main/java/com/ecoride/eureka_server/EurekaServerApplication.java
+++ src/main/java/com/ecoride/eureka_server/EurekaServerApplication.java
@@ -2,13 +2,12 @@ package com.ecoride.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaServerApplication.class, args);
+  }
 }
